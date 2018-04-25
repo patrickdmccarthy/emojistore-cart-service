9b4e5d703944b3379b0eff167c47e22fe41eff66
patrickdmccarthy <patrick.d.mccarthy@gmail.com>
2018-04-25 12:01:21 +0200

    allow cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+var cors = require('cors')
 
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
